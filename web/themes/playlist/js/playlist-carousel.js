36b856c21ec247ebad809832c172d41fd44ecafd
Joseph Ramos <jardix22@gmail.com>
2018-07-11 23:14:11 -0500

    Removed unnecessary console.

--- web/themes/playlist/js/playlist-carousel.js
+++ web/themes/playlist/js/playlist-carousel.js
@@ -1,8 +1,6 @@
 (function ($, Drupal, drupalSettings) {
   Drupal.behaviors.slider = {
     attach: function (context, settings) {
-      console.log('--Slick', $.slick);
-
       $('.playlist__videos').slick({
         infinite: true,
         slidesToShow: 3,
